dd069fd1255a34ed65a3f64cc2e237cdf1e80024
Levis <anton_levus@mail.ru>
2023-01-09 16:22:19 +0530

    refactoring

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,6 @@
-import { composeWithDevTools } from '@redux-devtools/extension';
 import { createStore } from 'redux';
 import todoReducer from './todoReducer';
 
-const store = createStore(todoReducer, composeWithDevTools());
+const store = createStore(todoReducer);
 
 export default store;
